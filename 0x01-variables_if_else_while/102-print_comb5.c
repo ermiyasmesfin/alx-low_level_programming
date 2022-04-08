6c8b25bba70301848af054c80ec977e7996f219f
Ermiyas <98635649+ermiyasmesfin@users.noreply.github.com>
2022-04-08 21:06:40 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,43 +8,26 @@
  */
 int main(void)
 {
-	int p = 48, q = 48, y = 48, x = 49;
+int num1, num2;
 
-	while (p < 58)
-	{
-		while (q < 58)
-		{
-			while (y < 58)
-			{
-				while (x < 58)
-				{
-					putchar(p);
-					putchar(q);
-					putchar(' ');
-					putchar(y);
-					putchar(x);
-					if (!(p == 57 &&
-						q == 56 &&
-						y == 57 &&
-						x == 57))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					x++;
-				}
-				y++;
-				x = 48;
-			}
-			q++;
-			q = p;
-			x = q + 1;
-		}
-		p++;
-		q = 48;
-		y = p;
-		x = q+ 1;
-	}
-	putchar('\n');
-	return (0);
+for (num1 = 0; num1 <=98; num1++)
+{
+for (num2 = num1 + 1; num2 <=99; num2++)
+{
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
+putchar(' ');
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
+
+if (num1 == 98 && num2 == 99)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+
+putchar('\n');
+
+return (0);
 }
