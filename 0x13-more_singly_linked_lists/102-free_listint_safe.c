face4c8b2ffb2dc648d11c99f28c0fb28486d87d
Ermiyas <98635649+ermiyasmesfin@users.noreply.github.com>
2022-05-24 20:19:49 +0300

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,38 +1,39 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * free_listint_safe - frees a linked list
- * @h: pointer to the first node in the linked list
- *
- * Return: number of elements in the freed list
+ * free_listint_safe - is a function that frees a linked list
+ * @h: pointer to the start of the list
+ * Return: number of elements in the list
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t len = 0;
-	int diff;
 	listint_t *temp;
+	int i;
+	size_t s;
 
-	if (!h || !*h)
+	if (h || *h == NULL)
 		return (0);
 
 	while (*h)
 	{
-		diff = *h - (*h)->next;
-		if (diff > 0)
+		i = *h - (*h)->next;
+
+		if (i > 0)
 		{
 			temp = (*h)->next;
+			free(*h);
 			*h = temp;
-			len++;
+			s++;
 		}
 		else
 		{
 			*h = NULL;
-			len++;
+			s++;
 			break;
 		}
 	}
 
 	*h = NULL;
-
-	return (len);
+	return (s);
 }
